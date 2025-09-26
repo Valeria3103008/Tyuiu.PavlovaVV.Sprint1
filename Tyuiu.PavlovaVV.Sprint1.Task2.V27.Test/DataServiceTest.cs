e2e540e12c70c43742e462dc9ea4fcf112bbc1ab
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-09-26 20:17:33 +0500

    Task2

---
+++ Tyuiu.PavlovaVV.Sprint1.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PavlovaVV.Sprint1.Task2.V27.Lib;
+namespace Tyuiu.PavlovaVV.Sprint1.Task2.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.CalculateSquarePerimetr(x);
+            Assert.AreEqual(8, res);
+        }
+    }
+}
---
+++ Tyuiu.PavlovaVV.Sprint1.Task2.V27/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Tyuiu.PavlovaVV.Sprint1.Task2.V27.Lib;
+namespace Tyuiu.PavlovaVV.Sprint1.Task2.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Квадрат числа X = " + ds.CalculateSquarePerimetr(x));
+
+            Console.ReadLine();
+        }
+    }
+}
