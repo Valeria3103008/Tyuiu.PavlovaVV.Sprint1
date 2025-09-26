585c700f8b6c41af6ac3160a21cacaf49df4dd39
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-09-26 20:36:39 +0500

    Task3

---
+++ Tyuiu.PavlovaVV.Sprint1.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PavlovaVV.Sprint1.Task3.V11.Lib;
+namespace Tyuiu.PavlovaVV.Sprint1.Task3.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x1 = -2, x2 = 1, x3 = 5, y1 = 5, y2 = 7, y3 = -3;
+            var res = ds.TriangleArea(x1, x2, x3, y1, y2, y3);
+            Assert.AreEqual(19, res);
+        }
+    }
+}
---
+++ Tyuiu.PavlovaVV.Sprint1.Task3.V11/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.PavlovaVV.Sprint1.Task3.V11.Lib;
+namespace Tyuiu.PavlovaVV.Sprint1.Task3.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Вершины-1, x2, x3, y1, y2 ,y3                                           *");
+            Console.WriteLine("***************************************************************************");
+
+            double x1, x2, x3, y1, y2, y3;
+
+            Console.WriteLine("Введите X1");
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите X2");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите X3");
+            x3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите Y1");
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите Y2");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите Y3");
+            y3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.TriangleArea(x1, x2, x3, y1, y2, y3));
+            Console.ReadLine();
+        }
+    }
+}
