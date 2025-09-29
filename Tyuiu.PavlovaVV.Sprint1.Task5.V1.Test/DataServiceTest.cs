964142cb1106859faa0292e4fb712dc48c1ad586
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-09-29 20:05:32 +0500

    Task5

---
+++ Tyuiu.PavlovaVV.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.PavlovaVV.Sprint1.Task5.V1.Lib;
+namespace Tyuiu.PavlovaVV.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x1 = 15;
+            double y1 = 5;
+            double x2 = 6;
+            double y2 = 10;
+            DataService ds = new DataService();
+            double res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+            int result = Convert.ToInt32(res);
+            int wait = 10;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.PavlovaVV.Sprint1.Task5.V1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.PavlovaVV.Sprint1.Task5.V1.Lib;
+namespace Tyuiu.PavlovaVV.Sprint1.Task5.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Ввелите координату X1:");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ввелите координату Y1:");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ввелите координату X2:");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ввелите координату Y2:");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = Convert.ToInt32(ds.DistanceBetweenDots(x1, y1, x2, y2));
+            Console.WriteLine("Расстояние между двумя точками с заданными координатами = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
